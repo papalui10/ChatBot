f8ee8dda6770e136bda18f5dbeee39b020b33b19
Luis Veliz <luisvelizjr@gmail.com>
2018-10-12 12:48:33 -0600

    Mad ChatBot project, created Controller , Runner and Model

---
+++ src/controller/ChatBotController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class ChatBotController
+{
+	public void start()
+	{
+		String userInput = JOptionPane.showInputDialog("What is your name?");
+		interactWithChatBot();
+	}
+	
+	public String interactWithChatBot(String userInput)
+	{
+		
+		
+		return null;
+	}
+
+}
